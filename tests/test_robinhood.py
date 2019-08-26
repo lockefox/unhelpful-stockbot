96253017c5dd0f109f864b71661f0da60b1f1903
Lockefox <locke.renard@gmail.com>
2019-08-26 14:55:31 -0700

    adding tests for price/info queries

--- tests/test_robinhood.py
+++ tests/test_robinhood.py
@@ -38,3 +38,38 @@ def test_get_error(rh_secrets):
             client_id=rh_secrets.robinhood_client_id,
         )
         quote = client.get('quotes/', params={'symbols': 'MU'})
+
+
+@pytest.fixture
+def rh_client(rh_secrets):
+    """fixture to DRY rh tests
+
+    Returns:
+        RobinhoodConnection: connection context
+
+    """
+    with robinhood.RobinhoodConnection(
+        username=rh_secrets.robinhood_username,
+        password=rh_secrets.robinhood_password,
+        client_id=rh_secrets.robinhood_client_id,
+    ) as client:
+        yield client
+
+
+class TestGetInfo:
+    """validates get_* queries and their weirdness"""
+
+    def test_get_price(self, rh_client):
+        price = robinhood.get_price('MU', rh_client)
+
+        assert price
+        assert isinstance(price, float)
+
+    def test_get_name(self, rh_client):
+        company_name = robinhood.get_name('MU', rh_client)
+
+        assert company_name == 'Micron Technology'
+
+    def test_get_name_failed(self, rh_client):
+        with pytest.raises(exceptions.TickerNotFound):
+            company_name = robinhood.get_name('FAKE', rh_client)
